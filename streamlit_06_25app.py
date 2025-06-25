ccebc4f592ed880c2289f4cc7c772a91633bdbdd
zhaoyage2015 <zhaoyage2015@outlook.com>
2025-06-25 14:38:17 +0800

    Update streamlit_06_25app.py

--- streamlit_06_25app.py
+++ streamlit_06_25app.py
@@ -60,16 +60,18 @@ if st.button("Predict"):
         fx = base_value + shap_contrib.sum()
 
         # 绘制 force plot
+        features = pd.Series(np.round(X_scaled[0], 3), index=feature_names)
+
+        # 绘制 force plot
         plt.clf()
         fig = plt.figure(figsize=(12, 3), dpi=600)
-        shap.force_plot(
-            base_value=base_value,
-            shap_values=shap_contrib,
-            features=X_scaled[0],  # 注意这里必须是 1D array 或 Series
-            feature_names=feature_names,
-            matplotlib=True,
-            show=False
-        )
+       shap.force_plot(
+       base_value=base_value,
+       shap_values=shap_contrib,
+       features=features,
+       feature_names=feature_names,
+       matplotlib=True,
+       show=False)
 
         st.caption(f"base: {base_value:.3f} + sum(SHAP): {shap_contrib.sum():.3f} = f(x): {fx:.3f}")
 
